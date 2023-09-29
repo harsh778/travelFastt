8b36cfc606aa0a5c1cb3c8f97b6272e8e9aca2d2
Aydin Ali <aydina2@illinois.edu>
2023-09-29 17:40:44 -0500

    Added empty home and login page to website

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from flask import Flask
 
+app = Flask("TravelFast")
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/login/')
+def test():
+    return "Login Page will go here"
