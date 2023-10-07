63eada2b91e1c5dc052cc5e9876264521eba9b25
Aydin Ali <aydina2@illinois.edu>
2023-10-06 19:23:34 -0500

    Added basic account creation and login frontend and backend

--- src/main.py
+++ src/main.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask("TravelFast")
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def redirect_to_login():
+    return render_template("login.html")
 
-@app.route('/login/')
-def test():
-    return "Login Page will go here"
+user_credentials = {"Bob": "bobiscool", "John": "password"} # Just a placeholder for the real database system for now
+
+@app.route('/login/', methods=["POST", "GET"])
+def login():
+    try:
+        username = request.form["username"]
+        password = request.form["password"]
+    except:
+        return render_template("login.html")
+    if username not in user_credentials:
+        return render_template("login.html", return_info = "Username not found!")
+    elif user_credentials[username] != password:
+        return render_template("login.html", return_info = "Incorrect password for this account! Please try again.")
+    else:
+        return render_template("home.html", user = username)
+    
+@app.route('/create_account/', methods=["POST", "GET"])
+def create_account():
+    try:
+        username = request.form["username"]
+        password = request.form["password"]
+    except:
+        return render_template("account_create.html")
+    if username in user_credentials:
+        return render_template("account_create.html", return_info = "This username already exists!")
+    else:
+        user_credentials[username] = password
+        return render_template("account_create.html", return_info = "Account succesfully created!")
