59944e98e10b04c9b4dca8660b3ea9b2ab26f810
Mike K <mikek8025@gmail.com>
2023-10-16 22:38:29 -0500

    Updated travel fast db schema based off of local testing

---
+++ db_defines/travel_fast_schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS `travelfast` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE 'travelfast';
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `routes` (
+  `id` int NOT NULL,
+  `latitude` mediumblob,
+  `longitude` mediumblob,
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
